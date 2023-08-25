656f65c426dbf9214c0165e9fb21b30e9a766b93
Omid Haqi <omid.haqi@outlook.com>
2023-08-25 14:15:49 +0330

    create thread by class

--- app/src/main/java/ir/umut/multiThreadingKotlin/MainActivity.kt
+++ app/src/main/java/ir/umut/multiThreadingKotlin/MainActivity.kt
@@ -2,10 +2,22 @@ package ir.umut.multiThreadingKotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    //by default mainActivity is main Thread.
+
+        Log.v("testThread",Thread.currentThread().name)
+
+
+
+    //method 1 => create class
+      val thread1 = Thread1()
+        thread1.start()
+
     }
 }
